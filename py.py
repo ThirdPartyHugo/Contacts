4273d3d249de5eac221db874ff594a35d2a62d70
ThirdPartyHugo <hpskate26@gmail.com>
2025-01-25 19:15:53 -0500

    ff

--- py.py
+++ py.py
@@ -131,4 +131,4 @@ def test_endpoint():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Use $PORT or default to 5000
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
