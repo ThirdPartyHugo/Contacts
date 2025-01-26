9e4fd7454d0b4b5cc826a21de3bf6c347c0f7661
ThirdPartyHugo <hpskate26@gmail.com>
2025-01-25 20:51:14 -0500

    rff

--- py.py
+++ py.py
@@ -115,5 +115,9 @@ def test_endpoint():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use $PORT or default to 5000
-    app.run(host="0.0.0.0", port=port)
+    if os.environ.get('FLASK_ENV') == 'production':
+        # Production specific behavior
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    else:
+        # Development specific behavior
+        app.run(debug=True)
